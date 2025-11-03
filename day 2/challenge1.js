87798f23d907888afbd70f864073939e941871d0
RD9870 <reemfarajd@gmail.com>
2025-11-03 16:52:25 +0200

    aded the challenge

--- day 2/challenge1.js
+++ day 2/challenge1.js
@@ -15,16 +15,16 @@ class Book{
         this.id = book_id;
         this.title = book_title; 
         this.author = book_author; 
-        this.available = isBookAvailable;
+        this.available = true;
     }
 }
 
 class Member{
     name; id; borrowed_books;
-    constructor(member_name, member_id, member_borrowed_books){
+    constructor(member_name, member_id){
         this.name = member_name; 
         this.id = member_id; 
-        this.borrowed_books = member_borrowed_books;
+        this.borrowed_books = [];
     }
 }
 
@@ -33,26 +33,80 @@ class Library{
     members = [];
 
 //add a book id 
-    add_book(book_id, book_title, book_author, isBookAvailable){
-        newBook = Book(book_id, book_title, book_author, isBookAvailable);
+    add_book(book_id, book_title, book_author){
+        let newBook = new Book(book_id, book_title, book_author);
+        this.books.push(newBook);
     }
 
-    register_member(member_name, member_id, member_borrowed_books){
-        newMember = Member(member_name, member_id, member_borrowed_books);
+    register_member(member_name, member_id){
+        let newMember = new Member(member_name, member_id);
+        this.members.push(newMember);
     }
 
 //add book id to know which book is being borrowed
     borrow_book(member_id,book_id){
-
+        let member = this.members.find((mem)=> mem.id == member_id);
+        let book = this.books.find((bk)=> bk.id == book_id);
+        if(book.available == true){
+            member.borrowed_books.push(book.id);
+            book.available = false;
+            console.log(`The Book ${book.title} has been added to the member's ${member.name} borrowed list`);
+        }
+        else{
+            console.log(`Sorry, this book ${book.title} is not Avaliable to add to the user ${member.name} right now`);
+        }
     }
 //add book id to know which book is being returned
-return_book(member_id){}
+return_book(member_id,book_id){
+        let member = this.members.find((mem)=> mem.id == member_id);
+        let bookinList = member.borrowed_books.find((bk)=> bk.id == book_id);
+        console.log('bookinList\n' + bookinList);
+        if(bookinList == null){
+            console.log(`Sorry, this member ${member.name} did not borrow this book`)
+        }
+        else if (bookinList != null){
+            member.borrowed_books.pop(bookinList.id);
+            bookinList.available = true;
+            console.log(`The member ${member.name} has returned this book`)
+        }
+}
 }
 
 
 function main(){
 
     lib = new Library;
-    lib.add_book("The Great Gatsby", "F. Scott Fitzgerald", true);
-    lib.register_member("John Doe", 1, []);
+    lib.add_book(1, "The Great Gatsby", "F. Scott Fitzgerald", true);
+    lib.add_book(2, "harry potter", "jkr", true);
+    lib.add_book(3, "percy jackson", "uncle ric", );
+    
+    lib.register_member("John Doe", 1);
+    lib.register_member("Emma Doe", 2);
+
+    console.log('let member borrow book')
+
+// emma borrows the great gatsby sucsess
+    lib.borrow_book(2,1)
+//jhon borrows the greate gatsby fails
+    lib.borrow_book(1,1)
+//emma borrows harry potter sucsess
+    lib.borrow_book(2,2)
+//john borrows percy jackson sucsess
+    lib.borrow_book(1,3)
+    
+    console.log(lib.members)
+    // console.log(lib.books)
+
+
+    console.log('let member return book')
+//emma returns percy jackson fails
+    lib.return_book(2,3);
+//emma returns  the great gatsby sucsess
+lib.return_book(2,1);
+    //         console.log(lib.members)
+    // console.log(lib.books)
+        console.log(lib.members)
+
 }
+
+main();
